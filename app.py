90ca081492a9ef41ec40e9b9e59222cd9857d362
ibozag <248069082+ibozag03-glitch@users.noreply.github.com>
2026-04-14 13:12:08 +0200

    Add Ismael Souleymane Gazobi to student list

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 # Liste des élèves — ajoutez votre ligne ici
 eleves = [
     {"prenom": "Loïc", "nom": "Dumont", "github": "loic-prof"},
+    {"prenom": "Ismael", "nom": "Souleymane Gazobi", "github": "ibozag03-glitch"}
+
 ]
 
 TEMPLATE = """
