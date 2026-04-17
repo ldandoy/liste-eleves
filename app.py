ec1d880310a8e7117ec1a76c9e81ddb86749304f
ibrahimdjidji250-afk <ibrahimdjidji250@gmail.com>
2026-04-17 14:54:17 +0200

    Update app.py
    ibrahimdjidji

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ eleves = [
      {"prenom": "Chahinez", "nom": "Boubekeur", "github": "mariabkr26", "groupe": "Groupe 1"},
     {"prenom": "Alake Faridath", "nom": "ABOGOURIN", "github": "faridathabogourin", "groupe": "Gr1"},
     {"prenom": "Nora", "nom": "Boukal", "github": "loic-prof"},
-    {"prenom": "Ibrahim", "nom": "DJIDJI", "github": "ibrahimdjidji250-afk", "groupe": "Gr1"},
+    {"prenom": "Ibrahim", "nom": "DJIDJI", "github": "ibrahimdjidji250-afk", "groupe": "Gr4"},
 ]
     
 TEMPLATE = """
