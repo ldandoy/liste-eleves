f5ec20706040b6bbcc57cdc1340f8adbda20e79f
ALOEHOUN Komi justin <komialoehoun01@gmail.com>
2026-04-17 13:02:03 +0200

    fix : Mes informations

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Liste des élèves — ajoutez votre ligne ici
 eleves = [
     {"prenom": "Loïc", "nom": "Dumont", "github": "loic-prof"},
-    {"prenom": "komi justin", "nom": "ALOEHOUN", "github": "komialoeoun01-beep"},
+    {"prenom": "komi justin", "nom": "ALOEHOUN", "github": "komialoeoun01-beep", "groupe": "1"},
 ]
 
 TEMPLATE = """
